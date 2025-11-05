0013cde6825cd5ec8af0e5ce144131351fd9f0ba
RIW-TL <149050983+RIW-TL@users.noreply.github.com>
2025-11-05 18:30:12 +0800

    Example.R

--- example.R
+++ example.R
@@ -1,13 +1,19 @@
+# It provides an example of running the above functions,
+# and specifically how to use our method.
+#--------------------------
+
 library(ncvreg)
 library(mvtnorm)
 library(glmnet)
 library(ks)
 library(bayesdistreg)
 #-----------------------------------------------
-p = 200;s = 10;K = 10
+
+# parameter setting
+p = 200;s = 10;K = 5;q = 2*s
 sig.beta = 1;sig.delta1 = sig.delta2 = 0.5
 
-n0 = 150;n1 = 600;n.vec = c(n0, rep(n1, K))
+n0 = 150;n1 = 1200;n.vec = c(n0, rep(n1, K))
 Sig.X = matrix(0,p,p)
 for (i in 1:p) {
   for (j in 1:p) {
@@ -15,11 +21,11 @@ for (i in 1:p) {
   }
 }
 
+size.A0 = 4;d = 4
+
 # example
-A = 1.5;M = 2;TU = 1
-size.A0 = 4;d = 8
 #------------------------------------------------  
-coef.all = Coef.gen(s, d = d, q = 2*s, size.A0 = size.A0,  
+coef.all = Coef.gen(s, d = d, q = q, size.A0 = size.A0,  
                     K = K,sig.beta = sig.beta,
                     sig.delta1 = sig.delta1, 
                     sig.delta2 = sig.delta1, 
@@ -44,19 +50,41 @@ for (k in 1:(K + 1)) {
   epsilon.k = rnorm (n.vec[k], 0, 1)
   y <- c(y, X[ind.k,] %*% B[, k] + epsilon.k)
 }    
-    
+
 #--------------------------------------------------------------------------
 ###########RIW-TL###################
-X0 = X[1:n0,];y0 = y[1:n0]
-fit = cv.ncvreg(X0,y0,family = "gaussian",penalty = "SCAD")
-beta.scad = coef(fit)[-1]
+fit = hyper.select.cv(X,y,n.vec,K,fold = 5)
 
-fit = fit.RIW(X,y,n.vec,K,A,M,TU,beta0.initial = beta.scad)
+A = fit$A.opt;
+M = fit$M.opt;
+TU = A + M;
+fit = fit.RIW(X,y,n.vec,K,A,M,TU)
 beta_RIW_TL = fit$beta_RIW_TL
-beta_RIW_TL_P = fit$beta_RIW_TL_P
+
+###########RIW-TL###################
+A = fit$A.opt.u;
+M = fit$M.opt.u;
+TU = A + M;
+fit = fit.RIW(X,y,n.vec,K,A,M,TU)
 beta_RIW_TL_U = fit$beta_RIW_TL_U
 
-SUR = fit$SUR      
-SUR_P = fit$SUR_P
-SUR_U = fit$SUR_U    
+###########Trans-Lasso#############
+prop.re1 <- Trans.lasso(X, y, n.vec, I.til = 1:(n.vec[1]/2), l1 = T)
+prop.re2 <- Trans.lasso(X, y, n.vec, I.til = (n.vec[1]/2 + 1):n.vec[1], l1=T)
+beta.prop <- (prop.re1$beta.hat + prop.re2$beta.hat)/2
+
+###########LASSO####################
+X0 = X[1:n0,];y0 = y[1:n0]
+fit = cv.glmnet(X0,y0,family = "gaussian",alpha = 1)
+beta.lasso = coef(fit)[-1]
+
+
+#-----------------------------------------------
+sum((beta_RIW_TL - beta0)^2)
+sum((beta.prop - beta0)^2)
+sum((beta.lasso - beta0)^2)
+
+
+
+
 
